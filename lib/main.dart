7acf83a0f87418ed094dbbdea37c318cae884ff9
Nandang Eka Prasetya <ekaprasetya2244@gmail.com>
2022-11-29 20:12:16 +0700

    ➕➕➕Update Dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
+import 'package:sp_util/sp_util.dart';
 
 import 'app/routes/app_pages.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Get.putAsync(
+    () => SpUtil.getInstance(),
+  );
+
   runApp(
     GetMaterialApp(
       title: "Application",
